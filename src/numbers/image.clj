872edf159bd0cb5c9abc2783ffcf5a70368f1a21
Derek Troy-West <derek@troywest.com>
2019-05-08 15:56:51 +1000

    update

--- src/numbers/image.clj
+++ src/numbers/image.clj
@@ -20,19 +20,25 @@
     (partition 4 (.getPixels raster 0 0 width height (int-array (* width height 4))))))
 
 (defn reading
-  [time type name long lat content]
-  {:time    time
-   :type    type
-   :name    name
-   :long    long
-   :lat     lat
-   :content content})
+  [time type name long lat value]
+  {:time  time
+   :type  type
+   :name  name
+   :long  long
+   :lat   lat
+   :value value})
 
 (defn scott-base
   [n]
-  (map (fn [idx]
-         (reading (+ (* idx 10000) 1557125670763) "ENG" "NZ-1" -78 166 (rand-int 3)))
-       (range n)))
+  (reduce into
+          []
+          (map (fn [idx]
+                 (let [time  (+ (* idx 10000) 1557125670763)
+                       value (rand-int 3)]
+                   [(reading time "ENG" "NZ-1" -78 166 value)
+                    (reading (+ 25 time) "ENG" "NZ-1" -78 166 value)
+                    (reading (+ 50 time) "ENG" "NZ-1" -78 166 value)]))
+               (range n))))
 
 (defn fuzz
   [secret idx station]
@@ -44,10 +50,19 @@
     (reduce-kv (fn [ret i item]
                  (let [time (+ start-at (* i 10000) (int (rand 8000)))
                        [e1 e2 e3] (map #(tx/words s-type %1) (take 3 item))]
-                   (conj ret
-                         (reading time s-type s-name s-long s-lat e1)
-                         (reading (+ time 25) s-type s-name s-long s-lat e2)
-                         (reading (+ time 50) s-type s-name s-long s-lat e3))))
+                   (case (:value (nth secret (* 3 idx)))
+                     0 (conj ret
+                             (reading time s-type s-name s-long s-lat e1)
+                             (reading (+ time 25) s-type s-name s-long s-lat e2)
+                             (reading (+ time 50) s-type s-name s-long s-lat e3))
+                     1 (conj ret
+                             (reading time s-type s-name s-long s-lat e2)
+                             (reading (+ time 25) s-type s-name s-long s-lat e3)
+                             (reading (+ time 50) s-type s-name s-long s-lat e1))
+                     2 (conj ret
+                             (reading time s-type s-name s-long s-lat e3)
+                             (reading (+ time 25) s-type s-name s-long s-lat e1)
+                             (reading (+ time 50) s-type s-name s-long s-lat e2)))))
                []
                (vec station))))
 
@@ -56,7 +71,9 @@
   (let [width    (width image)
         stations (partition width (pixels image))
         secret   (scott-base width)
-        readings (sort-by :time (reduce into [] (map-indexed (partial fuzz secret) stations)))]
+        readings (sort-by :time (reduce into [] (conj (map-indexed (partial fuzz secret) stations)
+                                                      (map (fn [reading]
+                                                             (update reading :value #(tx/words "ENG" %1))) secret))))]
     (into (vec (interleave readings
                            [{:time (+ 1 (:time (first readings))) :type "UXX" :name "X-RAY" :elts "base"}
                             {:time (+ 1 (:time (second readings))) :type "UXX" :name "X-RAY" :elts "base"}]))
